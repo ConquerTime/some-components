db7a46b6e70971d77c8db4000b857dcc11216280
ZYD <zhouydbit@gmail.com>
2024-11-01 19:07:54 +0800

    init

---
+++ components/Card/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import styles from './Card.module.scss';
+
+function Card() {
+  return (
+    // <div className={styles.wrapper}>Card</div>
+    <div>Card</div>
+  );
+}
+
+export default Card;
